91d8d3586320f1f3a8bf584d7424c3974659fdeb
Ahmmed Hisham <hisham1ahmmed@gmail.com>
2023-03-03 20:23:55 +0530

    modified

--- online/script.js
+++ online/script.js
@@ -1,26 +1,88 @@
-const userArray = [
-    {
-        userName:'user1',
-        userAdderss:"addr1",
-        userMark: 50
-    },
-    {
-        userName:'user2',
-        userAdderss:"addr2",
-        userMark: 50
-    },
-    {
-        userName:'user3',
-        userAdderss:"addr3",
-        userMark: 50
-    },
-    {
-        userName:'user1',
-        userAdderss:"addr1",
-        userMark: 50
-    }
-]
+// const userArray = [
+//     {
+//         userName:'user1',
+//         userAdderss:"addr1",
+//         userMark: 50
+//     },
+//     {
+//         userName:'user2',
+//         userAdderss:"addr2",
+//         userMark: 50
+//     },
+//     {
+//         userName:'user3',
+//         userAdderss:"addr3",
+//         userMark: 50
+//     },
+//     {
+//         userName:'user1',
+//         userAdderss:"addr1",
+//         userMark: 50
+//     }
+// ]
 
-const value = forEach(function (items){
-    console.log('My mark is ${}');
-}) 
+// const value = forEach(function (items){
+//     console.log('My mark is ${}');
+// }) 
+
+
+// spread opereator
+
+// const a ={
+//     key1:'value1',
+//     key2:'value2',
+    
+// };
+// const b = a;
+// b.key3 = 'value3'
+
+// console.log(a);
+
+// DESTUCTURING
+
+// const a ={
+//     key1:'value1',
+//     key2:'value2',
+//     key3: {
+//         nested1:"valueNested1"
+//     }  
+// };
+
+// const {
+//     key1,
+//     key2,
+//     key3
+// }=a
+
+// console.log(key1,key2,key3);
+
+// // Array desturctiong
+
+// const arr= [25,157,6]
+
+// const[val1,,val3] = arr
+// console.log(val1,val3);
+
+
+// OPTIONAL CHAINING
+
+// function test(){
+
+//     const obj = {
+//         key1: "hello",
+//         age:32,
+//         cls:null,
+//         details: {
+//             val1:'val2',
+//             val2:'val2'
+//         }
+//     }
+// }
+
+    // FUNTIONS
+
+    const MyArrown = () =>{
+        console.log("hello");
+    }
+
+     
