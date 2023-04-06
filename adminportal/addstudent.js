a3278ce6b26297404585264068b257869bfb8db2
Ahmmed Hisham <hisham1ahmmed@gmail.com>
2023-04-06 23:29:11 +0530

    study added

--- adminportal/addstudent.js
+++ adminportal/addstudent.js
@@ -1,5 +1,32 @@
-// const addStudent  = document.querySelector('.main')
+const studentName = document.querySelector(".name");
+const address = document.querySelector(".adress");
+const genter = document.querySelector(".Genter");
+const dob = document.querySelector(".dob");
+const addno = document.querySelector(".addno");
+const parent = document.querySelector(".parent");
+const grade = document.querySelector(".grade");
+const submit = document.querySelector(".submit");
+
+class AddingStudent {
+  constructor(name,adress, dob, bloodGroup) {
+    this.name = name;
+    this.address = adress
+    this.dateofbirth = dob;
+    this.bloodGroup = bloodGroup;
+  }
+
+  details() {
+    let date = new Date();
+    let age = date.getFullYear() - this.dateofbirth;
+    console.log(age);
+
+    return ` name:${this.dateofbirth}`;
+  }
+}
 
 
-
-// html = 
+submit.addEventListener("click",() =>{
+// studentName.innerHTML
+localStorage.setItem("studentname",studentName.value)
+})
+// localStorage.clear(")
---
+++ advanced-js-youtube/forloop-settime/script.js
@@ -0,0 +1,66 @@
+// let n = 1;
+// for (let i = 1; i < 5;i++) {
+  
+//   if (i == n){
+    
+//      }
+    
+//           setInterval(() => {
+//           console.log(n);
+          
+//           n++;
+//       }, 1000);
+//   }
+
+// let n =0
+//  while ( n < 5){
+//     if( n == i){
+//         setInterval(() => {
+//         console.log(n);
+//     }, 10);
+// }
+// n++
+// break
+//  }
+
+// for (var i = 1; i <= 3; i++) {
+//     setTimeout(function() { alert(i) }, 1000);
+// }
+
+// function doSetTimeout(i) {
+//     setTimeout(function() {
+//       console.log(i);
+//     }, 1000);
+//   }
+  
+//   for (var i = 1; i <= 5; i++){
+
+//       doSetTimeout(i);
+//   }
+
+// function animate(i){
+//     setTimeout(()=>{
+//         if(i==5){
+//             i=1
+//         }
+//         foo(i)
+//       animate(i+1)
+//     },1000)
+// }
+
+// let i=1; 
+
+
+// animate(i);
+// let j = 0;
+// function foo(hii){
+//   j = hii
+//   console.log(j); 
+// }
+
+
+let i = 0;
+do {
+  i += 1;
+
+} while (i < 5);    
