df5a9effb1005b01edc495479c534818fae347d4
Ahmmed Hisham <hisham1ahmmed@gmail.com>
2023-04-07 14:26:20 +0530

    recusive added

--- study/aaa -refernce/script.js
+++ study/aaa -refernce/script.js
@@ -25,3 +25,15 @@ http.createServer(function (req, res) {
     
 // })
 
+
+function foo(num){
+    if(num === 0) return 0
+    console.log(num);
+    return (function fooOf(num){
+        console.log(num);
+        
+        fooOf(num)
+        return fooOf(num - 1)
+    })
+}
+
