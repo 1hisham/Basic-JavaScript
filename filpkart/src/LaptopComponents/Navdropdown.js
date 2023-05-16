d6806787ca1893d891d0964892455b4725b45748
1hisham <hisham1ahmmed@gmail.com>
2023-05-16 21:59:20 +0530

    string

--- filpkart/src/LaptopComponents/Navdropdown.js
+++ filpkart/src/LaptopComponents/Navdropdown.js
@@ -1,100 +1,100 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-import DropdownInside from "../components/DropdownInside";
+// import React, { useState } from "react";
+// import styled from "styled-components";
+// import DropdownInside from "../components/DropdownInside";
 
-function Navdropdown(props) {
-  const [dropitem, setDropitem]
-  let mainitems = props.value;
-  console.log(mainitems, "this is main items");
+// function Navdropdown(props) {
+//   // const [dropitem, setDropitem]
+//   let mainitems = props.value;
+//   console.log(mainitems, "this is main items");
 
-  let fetchdata = async () => {
-    try {
-      const respose = await fetch(`./lapdata.json`);
-      const json = await respose.json();
-      setDropitem(json.laptopNav);
-    } catch (error) {
-      console.log("error", error);
-    }
-  };
+//   let fetchdata = async () => {
+//     try {
+//       const respose = await fetch(`./lapdata.json`);
+//       const json = await respose.json();
+//       setDropitem(json.laptopNav);
+//     } catch (error) {
+//       console.log("error", error);
+//     }
+//   };
 
-  ////get the find id and move to fetch
+//   ////get the find id and move to fetch
 
 
 
-  useEffect(() => {
-    fetchdata();
-  }, []);
+//   useEffect(() => {
+//     fetchdata();
+//   }, []);
 
-  let [subitem, setSubitem] = useState();
-  let [actives, setActives] = useState("");
-  let clickhandler = (item, index) => {
-    setSubitem(item);
-     console.log(index);
+//   let [subitem, setSubitem] = useState();
+//   let [actives, setActives] = useState("");
+//   let clickhandler = (item, index) => {
+//     setSubitem(item);
+//      console.log(index);
      
-    setActives("active");
-  };
-  let RemoveFn = () => {
-    setSubitem();
-  };
+//     setActives("active");
+//   };
+//   let RemoveFn = () => {
+//     setSubitem();
+//   };
 
-  console.log(subitem, "this is the sub item");
+//   console.log(subitem, "this is the sub item");
 
-  return (
-    <>
-      <Test
-        onMouseLeave={() => {
-          RemoveFn();
-        }}
-      >
-        <Toflex>
-          {/* {mainitems?.map((item, index) => {
-            return (
-              <MainList>
-                <List
+//   return (
+//     <>
+//       <Test
+//         onMouseLeave={() => {
+//           RemoveFn();
+//         }}
+//       >
+//         <Toflex>
+//           {/* {mainitems?.map((item, index) => {
+//             return (
+//               <MainList>
+//                 <List
                    
-                  className={ `index ${actives}`}
-                  onMouseOver={() => {
-                    clickhandler(item, index);
-                  }}
-                >
-                  {item.mainitems}
-                </List>
-              </MainList>
-            );
-          })} */}
-        </Toflex>
-        <DropdownInside value={subitem} />
-      </Test>
-    </>
-  );
-}
-const Test = styled.div`
-  background: #f9f9f9;
-  text-align: 400;
-  position: absolute;
-  z-index: 1;
-  display: flex;
-`;
-const MainList = styled.div``;
-const Toflex = styled.div`
-  box-shadow: 0 4px 16px 0 rgba(0, 0, 0, 0.2);
-  z-index: 2;
-`;
-const List = styled.li`
-  list-style: none;
-  display: flex;
-  align-items: center;
-  font-size: 14px;
-  padding: 12px;
-  min-height: 40px;
-  font-weight: 500;
-  min-width: 230px;
-  background: white;
-  &.active {
-    background-color: #f9f9f9;
-    color: #2874f0;
-    font-weight: 500;
-  }
-`;
+//                   className={ `index ${actives}`}
+//                   onMouseOver={() => {
+//                     clickhandler(item, index);
+//                   }}
+//                 >
+//                   {item.mainitems}
+//                 </List>
+//               </MainList>
+//             );
+//           })} */}
+//         </Toflex>
+//         <DropdownInside value={subitem} />
+//       </Test>
+//     </>
+//   );
+// }
+// const Test = styled.div`
+//   background: #f9f9f9;
+//   text-align: 400;
+//   position: absolute;
+//   z-index: 1;
+//   display: flex;
+// `;
+// const MainList = styled.div``;
+// const Toflex = styled.div`
+//   box-shadow: 0 4px 16px 0 rgba(0, 0, 0, 0.2);
+//   z-index: 2;
+// `;
+// const List = styled.li`
+//   list-style: none;
+//   display: flex;
+//   align-items: center;
+//   font-size: 14px;
+//   padding: 12px;
+//   min-height: 40px;
+//   font-weight: 500;
+//   min-width: 230px;
+//   background: white;
+//   &.active {
+//     background-color: #f9f9f9;
+//     color: #2874f0;
+//     font-weight: 500;
+//   }
+// `;
 
-export default Navdropdown;
+// export default Navdropdown;
--- stagejs/script.js
+++ stagejs/script.js
@@ -5,41 +5,39 @@
 //     res.write('Hello World!');
 //     res.end();
 // }).listen(8080);
-              
+
 // let data = {element: "barium"};
 
 // fetch("http://127.0.0.1:8080", {
 //   method: "POST",
-//   headers: {'Content-Type': 'application/json'}, 
+//   headers: {'Content-Type': 'application/json'},
 //   body: JSON.stringify(data)
 // }).then(res => {
 //   console.log("Request complete! response:", res);
 // });
 
-
 // fetch("http://127.0.0.1:8080")
 // .then(response => response.json())
 // .then(data=> {
 //     console.log(data);
-    
-// })
 
+// })
 
 // let num = 0
 //   function foo(num){
 //     console.log(num);
-    
+
 //     setInterval(() => {
-       
+
 //     }, 1000);
 //   }
 //   foo(num)
 
-// let i=1; 
+// let i=1;
 // function Itteration(i){
 //     myVar=setTimeout(function(){
 //        console.log(i);
-       
+
 //        if(i==5){
 //          i = 0
 //        }
@@ -49,15 +47,13 @@
 
 // Itteration(i);
 
-
 // let fetchData = () => {
 //    let item = fetch("https://raw.githubusercontent.com/1hisham/traing-oct/main/server/popularGiftsNow.json")
 //     .then(response => response.json())
 //     .then(data => {
 //         console.log(data);
 //         return data
-    
-    
+
 //     })
 //     return item
 // }
@@ -75,33 +71,31 @@
 //       }, 2000);
 //     });
 //   }
-  
+
 //   async function asyncCall() {
 //     console.log('calling');
 //     const result = await resolveAfter2Seconds();
 //     console.log(result);
 //     // Expected output: "resolved"
 //   }
-  
+
 //   asyncCall();
 //   console.log("1");
-  
+
 //   function fetchDemo() {
-    
+
 //     return fetch("https://raw.githubusercontent.com/1hisham/traing-oct/main/server/popularGiftsNow.json")
 //     .then((response)=> { return response.json();})
-//     .then((data) =>  { 
+//     .then((data) =>  {
 //         console.log(data);
-        
+
 //         return data;});
 // }
-
-
 
 // // fetchDemo().then(function(result) {
 // //     console.log(result);
 // //     console.log("hii");
-    
+
 // // });
 
 //  async function foo(){
@@ -109,10 +103,7 @@
 //      let itemdata = await fetchDemo()
 //     console.log(itemdata);
 //     console.log("iam here");
-    
-    
 
-    
 // }
 // console.log("5");
 // foo()
@@ -137,61 +128,191 @@
 
 // console.log(object);
 
-
 // object.push({
 //     id : 4 ,
 //     name :"jack",
 //     age:4,
 // })
 
-
 // ceser something
-// let number  = 105%26
-// console.log(number);
 
+// function toCecerCode(item,num){
+//   let ascciArray=[]
+//   let ceserarray = []
+//   let wordsarray = item.split("")
+//   console.log(wordsarray);
+//   if(num > 26){
+//     num = num%26
+//   }
 
-function toCecerCode(item,num){
-  let ascciArray=[]
-  let ceserarray = []
-  let wordsarray = item.split("")
-  console.log(wordsarray);
-  if(num > 26){
-    num = num%26
-  }
-  
-  wordsarray.forEach((word,idx) => {
-    let ascci =item.charCodeAt(idx)
-    console.log(ascci);
-    
-     let codedasci = (ascci+num)
-     if(codedasci > 90 && codedasci < 96){
+//   wordsarray.forEach((word,idx) => {
+//     let ascci =item.charCodeAt(idx)
+//     console.log(ascci);
 
-      let differ =codedasci - 90 
-      codedasci = 64 + differ
-     }
-     if(codedasci > 122){
-      let differ =codedasci - 122 
-      codedasci = 96 + differ
-      
-     }
-     console.log(codedasci);
-     
-    ascciArray.push(codedasci)
-    console.log(ascci);
-  })
-  ascciArray.forEach((code,idx) => {
-    let toWords = String.fromCharCode(code)
-    ceserarray.push(toWords)
+//      let codedasci = (ascci+num)
+//      if(codedasci > 90 && codedasci < 96){
 
-  })
-  console.log("ceser:",ceserarray);
-  let codedItem = ceserarray.join('')
-  return codedItem
-  
-  }
+//       let differ =codedasci - 90
+//       codedasci = 64 + differ
+//      }
+//      if(codedasci > 122){
+//       let differ =codedasci - 122
+//       codedasci = 96 + differ
 
-console.log(toCecerCode("A",104));
+//      }
+//      console.log(codedasci);
 
+//     ascciArray.push(codedasci)
+//     console.log(ascci);
+//   })
+//   ascciArray.forEach((code,idx) => {
+//     let toWords = String.fromCharCode(code)
+//     ceserarray.push(toWords)
+
+//   })
+//   console.log("ceser:",ceserarray);
+//   let codedItem = ceserarray.join('')
+//   return codedItem
+
+//   }
+
+// console.log(toCecerCode("A",104));
+
+// function fromCecercode(item,num){
+//   let ascciArray=[]
+//   let ceserarray = []
+//   let wordsarray = item.split("")
+//   console.log(wordsarray);
+
+//   wordsarray.forEach((word,idx) => {
+//     let ascci =item.charCodeAt(idx)
+//     ascciArray.push(ascci - num)
+//     console.log(ascci);
+//   })
+//   ascciArray.forEach((code,idx) => {
+//     let toWords = String.fromCharCode(code)
+//     ceserarray.push(toWords)
+
+//   })
+//   console.log("ceser:",ceserarray);
+//   let codedItem = ceserarray.join('')
+//   return codedItem
+
+// }
+// console.log(fromCecercode("jgnnq" , 3));
 
 // console.log("hello".charCodeAt(0));
 // console.log(String.fromCharCode(65));
+
+//  FETCHING CYPTOAPP API PROJECT
+
+// async function foo(){
+
+// const url = 'https://crypto-news-live9.p.rapidapi.com/news/CryptoNews';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '9e1fff0ea2mshe98c9fe2d38bd85p1ba772jsn548ce75befe5',
+// 		'X-RapidAPI-Host': 'crypto-news-live9.p.rapidapi.com'
+// 	}
+// };
+
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
+// }
+// foo()
+
+//     let fetchdata = async() =>{
+//         let response = await fetch("imagesgittest/home-icon-silhouette-svgrepo-com (1).png")
+//         let data = await response.json()
+//         console.log(data);
+//     }
+
+// console.log(fetchdata);
+
+// let pos = 0;
+// let count = 0
+// while(pos < array.length){
+//     if(array[pos] > array[pos + 1]){
+//         count++
+
+//         let itemOnPosition = array[pos]
+//         array.splice(pos,1,array[pos+1])
+//          array.splice(pos+1,1,itemOnPosition)
+//     }
+
+//     console.log(count);
+//     pos++
+// }
+
+// console.log(array);
+// for(let i= 0 ; i < array.length ; i++){
+//     let smallest = 0
+
+//     for(let j = i ; j < array.length ; j++){
+
+//     if(array[i] > array[j]){
+
+//       smallest = array[j]
+//     }
+// }
+// console.log(smallest);
+
+// // array.push(smallest)
+// }
+// let array = [5,7,2,8,3 ];
+// let i = 0
+// while( i < array.length){
+//     let smallest = 0
+//     let j = i
+//    while ( j <= array.length){
+//     if(array[i] > array[j]){
+//         smallest = array[i]
+//         array[i] = array[j]
+//         array[j] = smallest
+        
+//     }
+//     j++
+// }
+// count++
+
+//    i++
+// }
+// console.log(count);
+
+// console.log(array);
+
+let array =[2,5,6,8,9,1,4]
+let state = 1;
+
+function foo(ind) {
+  state = 1;
+  let i = 0;
+  while (i < array.length) {
+    if (array[i] > array[i + 1]) {
+      store = array[i];
+      array[i] = array[i + 1];
+      array[i + 1] = store;
+      state = 0;
+    }
+    i++;
+  }
+  return array;
+}
+
+  while (1) {
+      foo();
+
+      if (state == 1) {
+          console.log(array,"array");
+          
+          break;
+        }
+    }
+
+console.log(foo());
