e2b3bd5519bdcfd3342b99d6907acc741ab76618
1hisham <hisham1ahmmed@gmail.com>
2023-07-19 20:20:12 +0530

    add an object

--- objects/script.js
+++ objects/script.js
@@ -3,7 +3,7 @@
 let  object = {
     car : ["ford","tata", "toyota","suzuki"],
     bike: ["hero-honda","tvs","ktm","royalEnfield","bajaj"],
-    truck: ["barath Bens","eicher"]
+    truck: ["barath Bens"]
     bus: "Volvo",
     
 }
