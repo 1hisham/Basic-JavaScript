1bba21ed57cafbb14c939431cca6e2b1a04bea9e
1hisham <hisham1ahmmed@gmail.com>
2023-07-24 20:14:47 +0530

    birthDay problem

--- kus-birthday/script.js
+++ kus-birthday/script.js
@@ -1,42 +1,71 @@
-const birthDays =[
-       "1999-01-03",
-       "2001-01-05",
-       "2000-01-10",
-       "1998-01-21",
-]
+const birthDay = "2023-01-20";
+// "2001-01-07", "2000-02-27", "1998-01-29"
 //holiday includes second saturday , sandays
 const holidays = [
-    "2023-01-07" ,
-    "2023-01-10" ,
-    "2023-01-13" ,
-    "2023-01-14" ,
-    "2023-01-18" ,
-    "2023-01-19" ,
-    "2023-01-20" ,
-    "2023-01-21" ,
-    "2023-01-28" 
-]
+  "2023-01-02",
+  "2023-01-03",
+  "2023-01-04",
+  "2023-01-05",
+  "2023-01-06",
+  "2023-01-07",
+  "2023-01-08",
+  // "2023-01-09",
+  "2023-01-10",
+  // "2023-01-11",
+  "2023-01-12",
+  "2023-01-13",
+  "2023-01-14",
+  // "2023-01-15",
+  "2023-01-16",
+  "2023-01-17",
+  "2023-01-18",
+  "2023-01-19",
+  "2023-01-20",
+  "2023-01-21",
+  "2023-01-22",
+  "2023-01-23",
+  "2023-01-24",
+  "2023-01-25",
+  "2023-01-26",
+  // "2023-01-27",
+  "2023-01-28",
+  "2023-01-29",
+  "2023-01-30",
+  "2023-01-31",
+  "2023-02-05",
+  "2023-02-04",
+  "2023-02-02",
+  "2023-02-03",
 
-function checkBirthdayOnHoliday(birthdays, holidays){
-  birthdays.forEach((item) => {
-  let dayInThisYear = `2023-${item.slice(5,11)}`
-  let date = new Date(dayInThisYear)
-  let year = date.getFullYear()  
-  let month = date.getMonth()
-  let daysInMonths = daysInMonth(month +1 ,year)
-  let dayCount = date.getDay()
-  holidays.forEach((holiday) => {
-    console.log(dayInThisYear, holiday,"================",dayCount);
-    console.log(dayInThisYear ==  holiday,"================");
-    
-  })
-  console.log(dayCount);
-   
-   
- })
+];
+function setingTolastDayOfWeek(birthday, holidays) {
+ let date = new Date(birthday)
+ let birthDay = new Date(date.setDate(date.getDate())).toISOString().slice(0,10)
+ let suitableday = new Date(date.setDate(date.getDate())).toISOString().slice(0,10)
+ let toVaildDate = new Date(suitableday)
+ let toVaildDateDay = toVaildDate.getDay()
+ let i = 0
+ if( toVaildDateDay == 0){
+   suitableday = new Date(date.setDate(date.getDate()+ (6))).toISOString().slice(0,10)
+ }else{
+   suitableday = new Date(date.setDate(date.getDate()+ (6-toVaildDateDay))).toISOString().slice(0,10)
+ }
+ while(i < holidays.length){ 
+ let day = new Date(suitableday) 
+ let dateDay = day.getDay()
+  if(suitableday < birthDay){
+    suitableday = new Date(date.setDate(date.getDate()+ (6-dateDay)+7)).toISOString().slice(0,10)
+  }
+  if(dateDay == 0){
+    suitableday = new Date(date.setDate(date.getDate()+ 13)).toISOString().slice(0,10)
+  }
+  if(suitableday == holidays[i]){
+    suitableday = new Date(date.setDate(date.getDate()- 1)).toISOString().slice(0,10);
+    i=0
+  }
+i++
+}
+console.log(suitableday);
+}
+setingTolastDayOfWeek(birthDay, holidays) 
 
-}
-checkBirthdayOnHoliday(birthDays, holidays)
-function daysInMonth (month, year) {
-    return new Date(year, month, 0).getDate();
-}
