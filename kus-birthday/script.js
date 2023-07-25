5a20c2ebe9a713a08aed412c7f051c7bec4c91f0
1hisham <hisham1ahmmed@gmail.com>
2023-07-25 19:55:03 +0530

    todolist project

--- kus-birthday/script.js
+++ kus-birthday/script.js
@@ -1,17 +1,18 @@
-const birthDay = "2023-01-20";
+const birthDay = "2023-01-01";
 // "2001-01-07", "2000-02-27", "1998-01-29"
 //holiday includes second saturday , sandays
 const holidays = [
+  "2023-01-01",
   "2023-01-02",
   "2023-01-03",
   "2023-01-04",
   "2023-01-05",
   "2023-01-06",
   "2023-01-07",
-  "2023-01-08",
-  // "2023-01-09",
+  // "2023-01-08",
+  "2023-01-09",
   "2023-01-10",
-  // "2023-01-11",
+  "2023-01-11",
   "2023-01-12",
   "2023-01-13",
   "2023-01-14",
@@ -27,9 +28,9 @@ const holidays = [
   "2023-01-24",
   "2023-01-25",
   "2023-01-26",
-  // "2023-01-27",
-  "2023-01-28",
-  "2023-01-29",
+  "2023-01-27",
+  // "2023-01-28",
+  // "2023-01-29",
   "2023-01-30",
   "2023-01-31",
   "2023-02-05",
@@ -38,34 +39,33 @@ const holidays = [
   "2023-02-03",
 
 ];
-function setingTolastDayOfWeek(birthday, holidays) {
+function findSuitableDate(birthday, holidays) {
  let date = new Date(birthday)
- let birthDay = new Date(date.setDate(date.getDate())).toISOString().slice(0,10)
- let suitableday = new Date(date.setDate(date.getDate())).toISOString().slice(0,10)
- let toVaildDate = new Date(suitableday)
- let toVaildDateDay = toVaildDate.getDay()
- let i = 0
- if( toVaildDateDay == 0){
-   suitableday = new Date(date.setDate(date.getDate()+ (6))).toISOString().slice(0,10)
- }else{
-   suitableday = new Date(date.setDate(date.getDate()+ (6-toVaildDateDay))).toISOString().slice(0,10)
- }
+ let birthDay = date.toISOString().slice(0,10)
+ let suitableDate = birthDay
+ let toVaildDay = date.getDay()
+ if( toVaildDay == 0){
+   suitableDate = new Date(date.setDate(date.getDate()+ (6))).toISOString().slice(0,10)
+  }else{
+    suitableDate = new Date(date.setDate(date.getDate()+ (6-toVaildDay))).toISOString().slice(0,10)
+  }
+let i = 0
  while(i < holidays.length){ 
- let day = new Date(suitableday) 
- let dateDay = day.getDay()
-  if(suitableday < birthDay){
-    suitableday = new Date(date.setDate(date.getDate()+ (6-dateDay)+7)).toISOString().slice(0,10)
+ let suitableDateFormat = new Date(suitableDate) 
+ let day = suitableDateFormat.getDay()
+  if(suitableDate < birthDay){
+    suitableDate = new Date(date.setDate(date.getDate()+ (6-day)+7)).toISOString().slice(0,10)
   }
-  if(dateDay == 0){
-    suitableday = new Date(date.setDate(date.getDate()+ 13)).toISOString().slice(0,10)
+  if(day == 1){
+    suitableDate = new Date(date.setDate(date.getDate()+ 12)).toISOString().slice(0,10)
   }
-  if(suitableday == holidays[i]){
-    suitableday = new Date(date.setDate(date.getDate()- 1)).toISOString().slice(0,10);
+  if(suitableDate == holidays[i]){
+    suitableDate = new Date(date.setDate(date.getDate()- 1)).toISOString().slice(0,10);
     i=0
   }
 i++
 }
-console.log(suitableday);
+console.log(suitableDate);
 }
-setingTolastDayOfWeek(birthDay, holidays) 
+findSuitableDate(birthDay, holidays) 
 
