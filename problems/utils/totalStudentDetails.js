57f6fac07a41e5dad48d63ed719711bee8d064e0
1hisham <hisham1ahmmed@gmail.com>
2023-07-30 23:12:07 +0530

    added file problems

--- problems/utils/totalStudentDetails.js
+++ problems/utils/totalStudentDetails.js
@@ -5,13 +5,16 @@ function StudentDetails(classObj) {
     let studentsObject = {};
     let studentObject = {};
     let allsubject = [];
+    let initalMark = 0;
+    let totalMark = 0;
     classObj.students.forEach((student) => {
         let studentName = student.name;
         let studentId = student.id;
         student.marks.forEach((item) => {
             allsubject.push(item.subject);
+            totalMark = totalMark + item.mark;
         });
-        console.log(allsubject);
+        console.log(allsubject, totalMark);
         allsubject = [];
     });
     console.log(studentObject, allsubject);
--- problems/utils/totalStudentDetails.ts
+++ problems/utils/totalStudentDetails.ts
@@ -1,17 +1,20 @@
-function StudentDetails(classObj:ClassObj){
-    let studentsObject ={}
-    let studentObject:any = {}
-    let allsubject:any = [];
-    classObj.students.forEach((student)=> {
-    let studentName = student.name
-    let studentId = student.id
+function StudentDetails(classObj: ClassObj) {
+  let studentsObject = {};
+  let studentObject: any = {};
+  let allsubject: any = [];
+  let initalMark = 0;
+  let totalMark = 0;
+  classObj.students.forEach((student) => {
+    let studentName = student.name;
+    let studentId = student.id;
     student.marks.forEach((item) => {
-      allsubject.push(item.subject)
-    })
-    console.log(allsubject)
-    allsubject = []
-    })
-    console.log(studentObject,allsubject)
-    return studentsObject
+      allsubject.push(item.subject);
+      totalMark = totalMark+ item.mark
+    });
+    console.log(allsubject,totalMark);
+    allsubject = [];
+  });
+  console.log(studentObject, allsubject);
+  return studentsObject;
 }
-export {StudentDetails}
+export { StudentDetails };
