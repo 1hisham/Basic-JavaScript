d9cb3e9cd07fc3dc42376234cacc84e2809847fc
1hisham <hisham1ahmmed@gmail.com>
2023-08-13 20:56:25 +0530

    object

--- objects/script.js
+++ objects/script.js
@@ -2,7 +2,7 @@
 
 let  object = {
     car : ["ford","tata", "toyota"],
-    bike: ["hero-honda","tvs","ktm","royalEnfield"],
+    bike: ["hero-honda","tvs","ktm"],
     truck: ["barath Bens"]
     bus: "Volvo",
     
