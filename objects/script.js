9cbfbe0b5edfa5820b1a728026d9823d4d4a7ef1
1hisham <hisham1ahmmed@gmail.com>
2023-08-17 21:13:12 +0530

    objects

--- objects/script.js
+++ objects/script.js
@@ -1,9 +1,19 @@
 
 
-let  object = {
-    car : ["ford","tata", "toyota"],
-    bike: ["hero-honda","ktm"],
-    truck: ["barath Bens"]
-    bus: "Volvo",
+// let  object = {
+//     car : ["ford","tata", "toyota"],
+//     bike: ["hero-honda","ktm"],
+//     truck: ["barath Bens"]
+//     bus: "Volvo",
     
-}
+// }
+
+
+// let itemsObj = {
+//     first : 50,
+//     second: 12,
+//     total : function(a, b){return a + b}
+// }
+// console.log(itemsObj.total(20, 50))
+
+
