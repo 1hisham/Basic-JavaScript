aeceab4c782d1d5e3dd9a612782a5033a36a9eab
1hisham <hisham1ahmmed@gmail.com>
2023-08-20 21:15:28 +0530

    objects

--- objects/script.js
+++ objects/script.js
@@ -16,4 +16,10 @@
 // }
 // console.log(itemsObj.total(20, 50))
 
+// let itemsObj = {
+//     first : 50,
+//     second: 12,
+//     total : function(a, b){return a + b}
+// }
+// console.log(itemsObj.total(20, 50))
 
